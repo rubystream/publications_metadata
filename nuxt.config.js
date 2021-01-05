5bbf4aa05dd50f77ff7eeee6b05b79290e6c7e36
Dejan Dimic <dejan.dimic@gmail.com>
2021-01-06 00:00:06 +0100

    [#1] fix: Fix lint errors

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,11 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'OMA publication metadata Blog' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'OMA publication metadata Blog',
+      },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
@@ -43,7 +47,7 @@ export default {
   // BootstrapVue does not install the icons by default
   bootstrapVue: {
     // Install the `IconsPlugin` plugin (in addition to `BootstrapVue` plugin)
-    icons: true
+    icons: true,
   },
 
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
