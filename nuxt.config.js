b8bf3b664f29d4de0375342814f5fdc42bea41bd
Dejan Dimic <dejan.dimic@gmail.com>
2021-01-10 13:04:53 +0100

    [#11] fix: Liter error - missing comma

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ export default {
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
   router: {
-    base: '/publications_metadata/'
+    base: '/publications_metadata/',
   },
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
