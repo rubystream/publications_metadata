8fcd8b00a02478cda8cee90c4d89a56dbfe89cf1
rubystream <rubystream@users.noreply.github.com>
2021-01-10 12:13:07 +0000

    deploy: 91a5b0fc2ddaf9387a87676a3025655b29fad1c6

---
+++ _nuxt/static/1610280785/blog/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/blog", {data:[{articles:[{slug:"my-first-blog-post",description:"First impressions about OMA Publication Metadata system",title:"First Impressions",author:{name:"Dejan Dimic"},updatedAt:"2021-01-10T12:11:45.085Z"}]}],fetch:[],mutations:void 0});
